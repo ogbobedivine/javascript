d88a3f0ce8a807a502ebc96388d77fb1afb0f9ed
ogbobedivine <ogbobedivine2021@gmail.com>
2023-06-20 12:12:39 +0100

    Welcome to JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+// values and variables
+console.log("hello world!");
+
+let firstname = "donald";
+
+console.log(firstname);
+
+//variable naming conventions
+let last_name2 = "john";
+let $function = 50;
+
+console.log($function);
+console.log(last_name2);
